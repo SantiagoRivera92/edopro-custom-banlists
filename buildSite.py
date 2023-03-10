8f73f2e891abe1969f8d498c3da9301d46a7407d
Diamond Dude <santirivera92@gmail.com>
2023-03-10 02:37:40 +0100

    10/03/2023

--- buildSite.py
+++ buildSite.py
@@ -6,9 +6,9 @@ formatted = today.strftime("%d/%m/%Y")
 commitName = 'git commit -m \"%s\"'%formatted
 
 print('Generating Portuguese OTS pack exclusives', flush=True)
-os.system('python pt.py')
+os.system('python3.9 pt.py')
 print('Generating the actual banlist', flush=True)
-os.system('python lflistgen.py')
+os.system('python3.9 lflistgen.py')
 print('Generating cc++ banlist', flush=True)
 os.system('git add .')
 os.system(commitName)
